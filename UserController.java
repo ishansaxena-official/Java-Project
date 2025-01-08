aefe3143f5cab23bd5e7f1c1fa47e7d8e7c75622
Ishan Saxena <109264404+ishansaxena-official@users.noreply.github.com>
2025-01-08 17:54:27 +0530

    Create UserController.java

---
+++ UserController.java
@@ -0,0 +1,20 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public String registerUser(@RequestBody User user) {
+        return userService.registerUser(user);
+    }
+
+    @PostMapping("/login")
+    public String loginUser(@RequestBody User user) {
+        return userService.authenticateUser(user);
+    }
+}
