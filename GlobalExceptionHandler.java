8964ee41113552f203a262d1acc4ae9f6b160df4
Ishan Saxena <109264404+ishansaxena-official@users.noreply.github.com>
2025-01-08 17:55:00 +0530

    Create GlobalExceptionHandler.java

---
+++ GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleException(Exception e) {
+        return "Error: " + e.getMessage();
+    }
+}
